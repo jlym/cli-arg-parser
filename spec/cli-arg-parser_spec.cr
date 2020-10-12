4845007dcb6219d2c527adc1a0f5575e9a9be598
Jeffrey Lym <jeffreyalym@gmail.com>
2020-10-11 22:58:23 -0700

    Run crystal init lib cli-arg-parser

---
+++ spec/cli-arg-parser_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Cli::Arg::Parser do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/cli-arg-parser.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Cli::Arg::Parser`
+module Cli::Arg::Parser
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
